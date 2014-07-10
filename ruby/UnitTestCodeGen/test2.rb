bda352507f9ea4d438180a752d4f62264d9b4aa4
Swapnil Devikar <swap1712@gmail.com>
2014-07-10 11:31:35 -0400

    First fully working copy of the unit test code generator. Some refactoring will be required from this point One issue that needs to be fixed: Support for a variant of function signature declaration such as below: void fc_xmc_SaveRcvCatLabel(BYTE* arg1, BYTE* arg2) as against already supported void fc_xmc_SaveRcvCatLabel(BYTE*, BYTE*)

--- ruby/UnitTestCodeGen/test2.rb
+++ ruby/UnitTestCodeGen/test2.rb
@@ -2,9 +2,6 @@ require_relative 'scriptgenerator'
 
 generator = ScriptGenerator.new("resources/Unit testing template.xls")
 
-#generator.generate_metadata
-#generator.generate_test_data
-
-#puts generator.get_initlized_test_data()
-
-puts generator.get_params_as_struct_members
+generator.generate_metadata
+generator.generate_test_data
+generator.generate_test_code
